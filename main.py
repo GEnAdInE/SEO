c923ec78ae089a2e1bda5b73d38a8fccd9778185
GEnAdInE <one.robin@outlook.fr>
2022-02-12 16:17:18 +0100

    Error proof

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# list of account :
-# 57873murrilniyanna@randomail.io
-# evxopwhjqu@email.omshanti.edu.in
-
-
 import csv
 import sys
 
@@ -57,6 +52,12 @@ def stringConverter(oldStr: str):
         return int(preparedStr[0:-1])
     if oldStr == 'n/a':
         return -2
+    else:
+        try:
+            return int(preparedStr);
+        except:
+            print("an error append")
+
     return oldStr
 
 
@@ -95,84 +96,88 @@ with open(FilePath, 'r', encoding='utf8') as file:
 IndexArray = []  # array of the 10 first item where we want to find data
 i = 0
 for key in KeyWordArray:
-    if key.keywordDif == -1:
+    if key.keywordDif == -1 and key.googleScore > 0:
         IndexArray.append(i)
     i += 1
     if len(IndexArray) == 10:
         break
 
-# openning chrome and going to semrush.com
-driver = webdriver.Chrome()
-driver.get('https://www.semrush.com/projects/')
 
-title = driver.title
-if title.startswith('Dashboard'):  # already Login
-    print('we are good')
-else:
-    print('Login in ...') # logging in with the email and password passed when executing programs
-    driver.get('https://www.semrush.com/login/')
-    loginId = driver.find_element(By.ID, 'email')
-    loginId.clear()
-    loginId.send_keys(ListOfLogin[0])
-    loginPass = driver.find_element(By.ID, 'password')
-    loginPass.clear()
-    loginPass.send_keys(ListOfLogin[1])
-    loginPass.send_keys(Keys.ENTER)
+try:
+    # openning chrome and going to semrush.com
+    driver = webdriver.Chrome()
+    driver.get('https://www.semrush.com/projects/')
 
-    # waiting for page to load up
-    try:
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="srf-searchbar"]/form/div/div[2]/div[1]/input')))
-    except:
-        print('took too long')
-        Quit()
+    title = driver.title
+    if title.startswith('Dashboard'):  # already Login
+        print('we are good')
+    else:
+        print('Login in ...') # logging in with the email and password passed when executing programs
+        driver.get('https://www.semrush.com/login/')
+        loginId = driver.find_element(By.ID, 'email')
+        loginId.clear()
+        loginId.send_keys(ListOfLogin[0])
+        loginPass = driver.find_element(By.ID, 'password')
+        loginPass.clear()
+        loginPass.send_keys(ListOfLogin[1])
+        loginPass.send_keys(Keys.ENTER)
 
-# main part to gather the data
-for index in IndexArray:
+        # waiting for page to load up
+        try:
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="srf-searchbar"]/form/div/div[2]/div[1]/input')))
+        except:
+            print('took too long')
+            Quit()
 
-    # searching for the word
-    driver.get('https://www.semrush.com/analytics/keywordoverview/?q=' + KeyWordArray[index].keyword + '&db=fr')
+    # main part to gather the data
+    for index in IndexArray:
 
-    # waiting for page to load
-    try:
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="app"]/div/div/div[4]/div[2]/div/div[1]/div/div/div[1]/span[2]/span')))
-    except:
-        print('error to get data')
+        # searching for the word
+        driver.get('https://www.semrush.com/analytics/keywordoverview/?q=' + KeyWordArray[index].keyword + '&db=fr')
 
-    # check if we already used our 10 word per day
-    if check_exists_by_xpath('/html/body/div[3]/div/div/div/div/div/div[1]'):
-        print("Trial period pop up")
-        break;
+        # waiting for page to load
+        try:
+            WebDriverWait(driver, 6).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '/html/body/div[1]/main/div/div/div/div[4]/div[2]/div/div[1]/div/div/div[1]/span[2]/span')))
+        except:
+            print('error to get data')
 
-    # gathering data
-    try:
-        el = driver.find_element(By.XPATH,
-                                 '//*[@id="app"]/div/div/div[4]/div[2]/div/div[1]/div/div/div[1]/span[2]/span')
-        KeyWordArray[index].volume = stringConverter(el.text)
-    except:
-        print("No data for Volume")
-        KeyWordArray[index].volume = -2
+        # check if we already used our 10 word per day
+        if check_exists_by_xpath('/html/body/div[3]/div/div/div/div/div/div[1]'):
+            print("Trial period pop up")
+            break;
 
-    try:
-        el = driver.find_element(By.XPATH,
-                                 '//*[@id="app"]/div/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/span')
-        KeyWordArray[index].globalVolume = stringConverter(el.text)
-    except:
-        print("No data for Global Volume")
-        KeyWordArray[index].globalVolume = -2
+        # gathering data
+        try:
+            el = driver.find_element(By.XPATH,
+                                     '/html/body/div[1]/main/div/div/div/div[4]/div[2]/div/div[1]/div/div/div[1]/span[2]/span')
+            KeyWordArray[index].volume = stringConverter(el.text)
+        except:
+            print("No data for Volume")
+            KeyWordArray[index].volume = -2
 
-    try:
-        el = driver.find_element(By.XPATH,
-                                 '//*[@id="app"]/div/div/div[4]/div[2]/div/div[1]/div/div/div[2]/span[2]/div/div[1]/span[1]')
-        KeyWordArray[index].keywordDif = stringConverter(el.text)
-    except:
-        print("No data for difficulty")
-        KeyWordArray[index].keywordDif = -2
+        try:
+            el = driver.find_element(By.XPATH,
+                                     '/html/body/div[1]/main/div/div/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/span')
+            KeyWordArray[index].globalVolume = stringConverter(el.text)
+        except:
+            print("No data for Global Volume")
+            KeyWordArray[index].globalVolume = -2
 
-# end of program
-print("Data have been gathered")
+        try:
+            el = driver.find_element(By.XPATH,
+                                     '/html/body/div[1]/main/div/div/div/div[4]/div[2]/div/div[1]/div/div/div[2]/span[2]/div/div[1]/span[1]')
+            KeyWordArray[index].keywordDif = stringConverter(el.text)
+        except:
+            print("No data for difficulty")
+            KeyWordArray[index].keywordDif = -2
+
+    # end of program
+    print("Data have been gathered")
+except:
+    print("an error related to chrome driver append, saving data")
 
 # to export back to CSV we need an array of array
 ExportArray = []
